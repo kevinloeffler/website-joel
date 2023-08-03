f4d19977375f2be88ca5532df19ca73201e3d85c
kevinloeffler <hi@kevinloeffler.ch>
2023-08-03 23:07:37 +0200

    Fixed blue text in navigation overlay

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -75,9 +75,9 @@
         top: 0;
         left: 0;
         right: 0;
-        bottom: 0;
+        /*bottom: 0;*/
+        height: 100dvh;
         z-index: 99;
-
         background-color: white;
     }
 
@@ -135,6 +135,7 @@
     }
 
     button {
+        color: var(--text-color);
         border: none;
         background-color: transparent;
     }
