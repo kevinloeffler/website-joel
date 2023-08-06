597dc4ca5721f63666b20b15292fff0e4099daad
kevinloeffler <hi@kevinloeffler.ch>
2023-08-06 12:31:06 +0200

    Add newlines

--- update-project.sh
+++ update-project.sh
@@ -4,21 +4,27 @@ echo "------------ UPDATE PROJECT ------------"
 
 echo "1) COMPOSE DOWN................"
 sudo docker compose down
+echo ""
 
 echo "2) FETCH...................."
 git fetch
+echo ""
 
 echo "3) RESET LOCAL CHANGES......"
 git reset --hard origin/master
+echo ""
 
 echo "4) PERMISSIONS.............."
 chmod 755 update-project.sh
+echo ""
 
 echo "5) BUILD...................."
 npm run build
+echo ""
 
 echo "6) COMPOSE UP.................."
 sudo docker compose build
 sudo docker compose up -d
+echo ""
 
 echo "----------------- DONE -----------------"
