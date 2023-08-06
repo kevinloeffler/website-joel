1b81380a4ad537d1a5305376c45d5b1cbd0d92df
kevinloeffler <hi@kevinloeffler.ch>
2023-08-06 12:17:20 +0200

    Add docker compose to update script

--- update-project.sh
+++ update-project.sh
@@ -2,16 +2,22 @@
 
 echo "------------ UPDATE PROJECT ------------"
 
-echo "1) FETCH"
+echo "COMPOSE DOWN................"
+sudo docker compose down
+
+echo "1) FETCH...................."
 git fetch
 
-echo "2) RESET LOCAL CHANGES"
+echo "2) RESET LOCAL CHANGES......"
 git reset --hard origin/master
 
-echo "3) PERMISSIONS"
+echo "3) PERMISSIONS.............."
 chmod 777 update-project.sh
 
-echo "4) BUILD"
+echo "4) BUILD...................."
 npm run build
 
-echo "------------ DONE ------------"
+echo "COMPOSE UP.................."
+sudo docker compose up -d
+
+echo "----------------- DONE -----------------"
